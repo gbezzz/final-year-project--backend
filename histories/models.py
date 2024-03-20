be0a05a1bec5bf874bcefc2a549efc90b383d02a
Prince Gbezeh <gbezehp@gmail.com>
2024-03-20 19:43:59 +0000

    added timestamp to generated report (also deleted histories app)

--- histories/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from recommendations.models import Diagnose
-
-
-# Create your models here.
-class Report(models.Model):
-    diagnose = models.ForeignKey(Diagnose, on_delete=models.CASCADE)
