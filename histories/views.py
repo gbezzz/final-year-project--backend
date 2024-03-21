e746143f150a1032441b5d963bdfa0393dbcd29f
Prince Gbezeh <gbezehp@gmail.com>
2024-03-21 17:52:00 +0000

    Added permissions and filters to recommendations app and histories app

--- histories/views.py
+++ histories/views.py
@@ -2,10 +2,19 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import History
 from .serializers import HistorySerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
 
 class HistoryViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = History.objects.all()
+
+    def get_queryset(self):
+
+        if self.request.user.is_superuser:
+            return History.objects.all()
+        return History.objects.filter(doctor=self.request.user)
+
     serializer_class = HistorySerializer
+    permission_classes = IsAuthenticated
--- recommendations/views.py
+++ recommendations/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Patient, Diagnose
 from .serializers import PatientSerializer, DiagnoseSerializer, ReportSerializer
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -9,11 +10,19 @@ from .serializers import PatientSerializer, DiagnoseSerializer, ReportSerializer
 class PatientViewSet(viewsets.ModelViewSet):
     queryset = Patient.objects.all()
     serializer_class = PatientSerializer
+    permission_classes = IsAuthenticated
 
 
 class DiagnoseViewSet(viewsets.ModelViewSet):
     queryset = Diagnose.objects.all()
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return Diagnose.objects.all()
+        return Diagnose.objects.filter(doctor=self.request.user)
+
     serializer_class = DiagnoseSerializer
+    permission_classes = IsAuthenticated
 
     def perform_create(self, serializer):
         # Set the doctor's details from the request user
@@ -27,4 +36,12 @@ class DiagnoseViewSet(viewsets.ModelViewSet):
 
 class ReportViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Diagnose.objects.all()
+
+    def get_queryset(self):
+
+        if self.request.user.is_superuser:
+            return Diagnose.objects.all()
+        return Diagnose.objects.filter(doctor=self.request.user)
+
     serializer_class = ReportSerializer
+    permission_classes = IsAuthenticated
