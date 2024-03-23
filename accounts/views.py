36807c663b8da3305322df6eaf43c3bb99dd5320
Prince Gbezeh <gbezehp@gmail.com>
2024-03-23 00:41:18 +0000

    Implemented JWTs and updated requirements.txt

--- accounts/views.py
+++ accounts/views.py
@@ -4,9 +4,36 @@ from rest_framework import viewsets
 from .serializers import UserSerializer
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework import generics
+from dj_rest_auth.views import LoginView as BaseLoginView
+from dj_rest_auth.registration.views import RegisterView as BaseRegisterView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
 # Create your views here.
+
+
+class LoginView(BaseLoginView):
+    def get_response(self):
+        original_response = super().get_response()
+        refresh = RefreshToken.for_user(self.user)
+        original_response.data["access"] = str(refresh.access_token)
+        original_response.data["refresh"] = str(refresh)
+
+        # Remove the token created by dj-rest-auth automatically from the response
+        if "key" in original_response.data:
+            del original_response.data["key"]
+        return original_response
+
+
+class RegisterView(BaseRegisterView):
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        # Remove the token created by dj-rest-auth automatically from the response
+        if "key" in response.data:
+            del response.data["key"]
+        return response
+
+
 class UserViewSet(viewsets.ModelViewSet):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
--- django_project/urls.py
+++ django_project/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -26,4 +30,6 @@ urlpatterns = [
     path("api/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
     path("api/recommendations/", include("recommendations.urls")),
     path("api/report-history/", include("histories.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
